b2927764119163a552a82f814c66e6ef580c3b96
Diego GTG <diegogomeztg@outlook.com>
2026-06-23 18:44:06 -0600

    userbadge on users

--- src/app/Components/vista-usuarios/vista-usuarios.ts
+++ src/app/Components/vista-usuarios/vista-usuarios.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../Services/user-service';
 import { Usuario } from '../../Interfaces/usuario';
-import { CommonModule } from '@angular/common'; // <-- Importante
+import { CommonModule } from '@angular/common'; 
 import Swal from 'sweetalert2';
+import { UserBadgeComponent } from '../user-badge-component/user-badge-component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../Services/auth-service';
 
 @Component({
   selector: 'app-vista-usuarios',
-  standalone: true, // <-- Aseguramos que esté marcado como standalone
-  imports: [CommonModule], // <-- Añadido aquí
+  standalone: true,
+  imports: [CommonModule, UserBadgeComponent],
   templateUrl: './vista-usuarios.html',
   styleUrl: './vista-usuarios.css',
 })
 export class VistaUsuarios implements OnInit {
   usuarios: Usuario[] = [];
 
-  constructor(private usuarioService: UserService) {}
+  miRol: string | null = null;
+  token: string | null = null;
+  username: string | null = null;
+  idUsuario: number | null = null;
+  usuarioSesion: any;
+
+  constructor(
+    private usuarioService: UserService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+    this.miRol = this.authService.getUserRol();
+    this.token = this.authService.getToken();
+    this.username = this.authService.getUsername();
+    this.idUsuario = Number(this.authService.getIdUsuario());
+    this.usuarioSesion = { nombre: this.username, rol: this.miRol };
     this.cargarUsuarios();
   }
 
@@ -24,14 +42,9 @@ export class VistaUsuarios implements OnInit {
     this.usuarioService.getAllUsers().subscribe({
       next: (result) => {
         if (result.correct) {
-          // Si tu backend devuelve las propiedades dentro de 'objects' directamente
           this.usuarios = result.objects;
-          
-          Swal.fire(
-            '¡Éxito!',
-            'Se han obtenido todos los usuarios correctamente.',
-            'success',
-          );
+
+          Swal.fire('¡Éxito!', 'Se han obtenido todos los usuarios correctamente.', 'success');
         } else {
           Swal.fire('Error', 'Algo salió mal, no se pudo traer a los usuarios.', 'error');
         }
